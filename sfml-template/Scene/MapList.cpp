6acba455c8c00343c810825bfcb9d68d8777ef7a
sowsky652 <qudrnjs98@gmail.com>
2022-11-08 12:02:18 +0900

    Maplist추가

--- sfml-template/Scene/MapList.cpp
+++ sfml-template/Scene/MapList.cpp
@@ -1,1 +1,33 @@
 #include "MapList.h"
+
+MapList::MapList()
+{
+}
+
+MapList::~MapList()
+{
+}
+
+void MapList::Init()
+{
+}
+
+void MapList::Release()
+{
+}
+
+void MapList::Enter()
+{
+}
+
+void MapList::Update(float dt)
+{
+}
+
+void MapList::Exit()
+{
+}
+
+void MapList::Draw(RenderWindow& window)
+{
+}
--- sfml-template/Scene/MapList.h
+++ sfml-template/Scene/MapList.h
@@ -1,6 +1,21 @@
-#pragma once
-class MapList
+﻿#pragma once
+#include"Scene.h"
+
+class MapList :public Scene
 {
+public:
+	MapList();
+	virtual ~MapList();
+
+	virtual void Init();	// ����
+	virtual void Release();		// ����
+
+	virtual void Enter();
+	virtual void Update(float dt);
+	virtual void Exit();
+	virtual void Draw(RenderWindow& window);
+
+private:
 
 };
 
