bc0ef207a031e0096d3bff8cfa3c5481c25f5d9d
gh1928 <oos1928@gmail.com>
2022-12-03 17:40:49 +0900

    와이어 시각화

--- sfml-template/Objects/WireableObject.cpp
+++ sfml-template/Objects/WireableObject.cpp
@@ -3,11 +3,14 @@
 #include "../FrameWork/InputMgr.h"
 #include "../FrameWork/Utils.h"
 #include "../FrameWork/Const.h"
+#include "../Objects/Button.h"
 //#include "../Objects/NumBox.h"
+
+Phase WireableObject::phase = Phase::None;
 
 WireableObject::WireableObject()
 {
-	
+
 }
 
 WireableObject::~WireableObject()
@@ -20,12 +23,22 @@ WireableObject::~WireableObject()
 	//	}
 	//}
 	//numbox.clear();
+
+	if (!wires.empty())
+	{
+		for (auto w : wires)
+			delete w;
+
+		wires.clear();
+	}
 }
 
 void WireableObject::Init()
 {
 	Object::Init();
 	SetWireCheckBox();
+	orange = RESOURCEMGR->GetTexture("Graphics/orange.png");
+	blue = RESOURCEMGR->GetTexture("Graphics/blue.png");
 }
 
 void WireableObject::Update(float dt)
@@ -57,16 +70,25 @@ void WireableObject::SetWireCheckBox()
 }
 
 void WireableObject::WireModDraw(RenderWindow& window)
-{	
+{
 	isMouseIn = false;
 	Vector2f mouseWorldPos = window.mapPixelToCoords((Vector2i)InputMgr::GetMousePos(), window.getView());
 	isMouseIn = wireCheckBox.getGlobalBounds().contains(mouseWorldPos);
 
 	wireCheckBox.setFillColor(isMouseIn ? Color(248, 147, 30) : Color(37, 255, 254));
 
+	if(isWiring)
+		DrawWire(window);	
+
 	if (isWiring && isInMapTool)
-	{		
-		window.draw(wireCheckBox);
+	{
+		if (phase == Phase::TriggerSelect
+			&& type == ObjectType::Trigger)
+			window.draw(wireCheckBox);
+
+		if (phase == Phase::CatcherSelect
+			&& type == ObjectType::Catcher)
+			window.draw(wireCheckBox);
 	}
 
 	//if (!numbox.empty() && isWiring)
@@ -76,6 +98,44 @@ void WireableObject::WireModDraw(RenderWindow& window)
 	//		i->Draw(window);
 	//	}
 	//}
+}
+
+void WireableObject::DrawWire(RenderWindow& window)
+{
+	if (isMouseIn && type == ObjectType::Trigger
+		&& phase == Phase::TriggerSelect
+		&& InputMgr::GetMouseButtonDown(Mouse::Left))
+	{
+		SetPhase(Phase::CatcherSelect);
+		Button* temp = (Button*)this;
+		wires.push_back(new Wire);
+		wires.back()->buttonNum = temp->GetButtonId();
+		wires.back()->isConnected = false;
+		wires.back()->wire.setPrimitiveType(Lines);
+		wires.back()->wire.resize(2);
+	}
+
+	if (phase == Phase::CatcherSelect
+		&& InputMgr::GetMouseButtonDown(Mouse::Right))
+	{
+		SetPhase(Phase::TriggerSelect);
+		delete wires.back();
+		wires.pop_back();
+	}
+
+	if (wires.empty())
+		return;
+
+	wires.back()->wire[0].position = position;
+	wires.back()->wire[1].position
+		= window.mapPixelToCoords((Vector2i)InputMgr::GetMousePos(), window.getView());
+
+
+	for (auto w : wires)
+	{
+		window.draw(w->wire, w->isConnected ? orange : blue);			
+	}
+
 }
 
 //void WireableObject::AddNumBox(NumBox* nb)
@@ -115,7 +175,7 @@ void WireableObject::WireModDraw(RenderWindow& window)
 list<int> WireableObject::GetWireListFromMapTool()
 {
 	list<int> temp;
-	
+
 	//for (auto boxnum : numbox)
 	//{
 	//	temp.push_back(boxnum->GetNum());
